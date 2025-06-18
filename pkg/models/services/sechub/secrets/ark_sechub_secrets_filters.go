4b1b87ff2ce58743de45473894c571fe41038890
Joe Strickland <joseph.a.strickland@gmail.com>
2025-06-18 13:15:39 -0400

    Added validation for flags

--- pkg/models/services/sechub/secrets/ark_sechub_secrets_filters.go
+++ pkg/models/services/sechub/secrets/ark_sechub_secrets_filters.go
@@ -2,9 +2,9 @@ package secrets
 
 // ArkSecHubSecretsFilter represents the filter options for accounts.
 type ArkSecHubSecretsFilter struct {
-	Projection string `json:"projection,omitempty" mapstructure:"projection,omitempty" desc:"Whether to use extended projection or not" flag:"projection"`
+	Projection string `json:"projection,omitempty" mapstructure:"projection,omitempty" desc:"Whether to use extended projection or not - Allowed Values: 'EXTEND','REGULAR'" flag:"projection" validate:"oneof=EXTEND REGULAR"`
 	Filter     string `json:"filter,omitempty" mapstructure:"filter,omitempty" desc:"Filter to apply" flag:"filter"`
 	Sort       string `json:"sort,omitempty" mapstructure:"sort,omitempty" desc:"Sort results by given key" flag:"sort"`
-	Offset     int    `json:"offset,omitempty" mapstructure:"offset,omitempty" desc:"Offset to the accounts list" flag:"offset"`
-	Limit      int    `json:"limit,omitempty" mapstructure:"limit,omitempty" desc:"Limit of results" flag:"limit"`
+	Offset     int    `json:"offset,omitempty" mapstructure:"offset,omitempty" desc:"Offset to the accounts list (min=0,max=1500000)" flag:"offset" default:"0" validate:"min=0,max=1500000"`
+	Limit      int    `json:"limit,omitempty" mapstructure:"limit,omitempty" desc:"Limit of results (min=1,max=1000)" flag:"limit" validate:"min=1,max=1000"`
 }
